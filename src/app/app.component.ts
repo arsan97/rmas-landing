30d059e7a8f3356aec4ca82ea0580eeeb3a7cabc
Anbarsan P <anbarsan.p@accolitedigital.com>
2022-07-08 11:25:42 +0530

    Added SVG image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'rmas-landing';
+
+  ngAfterViewInit(): void {
+    let collection = document.getElementsByTagName('linearGradient');
+    let data: any = [];
+    Array.from(collection).forEach((element) => {
+      let x = element.getAttribute('x2');
+      let y = element.getAttribute('y2');
+      data.push({ x: x, y: y });
+    });
+    console.log(data);
+  }
 }
