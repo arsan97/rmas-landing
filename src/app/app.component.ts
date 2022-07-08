e5d5c60f0cd07dd0207b2ec5f7fa882b08cf52ba
Anbarsan P <anbarsan.p@accolitedigital.com>
2022-07-08 13:14:00 +0530

    Parse xml data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
@@ -6,17 +7,36 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rmas-landing';
+  svgImage: any;
 
-  ngAfterViewInit(): void {
-    let collection = document.getElementsByTagName('linearGradient');
+  constructor(
+    private httpClient: HttpClient,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  getCoordinates(xml: any) {
+    const elements = xml.getElementsByTagName('linearGradient');
     let data: any = [];
-    Array.from(collection).forEach((element) => {
+    Array.from(elements).forEach((element: any) => {
       let x = element.getAttribute('x2');
       let y = element.getAttribute('y2');
       data.push({ x: x, y: y });
     });
     console.log(data);
+  }
+
+  ngOnInit(): void {
+    this.httpClient
+      .get(`assets/images/Map_with_locationpointer_lines.svg`, {
+        responseType: 'text',
+      })
+      .subscribe((value) => {
+        this.svgImage = this.sanitizer.bypassSecurityTrustHtml(value);
+        let parser = new DOMParser();
+        let xml = parser.parseFromString(value, 'text/xml');
+        this.getCoordinates(xml);
+      });
   }
 }
